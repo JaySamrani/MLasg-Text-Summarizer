b042c1b486233fad98cbf8db98482da9a1bef59a
Manjeet9812 <111739529+Manjeet9812@users.noreply.github.com>
2023-12-07 16:58:31 +0530

    Add files via upload

---
+++ Post-processing/decoding.py
@@ -0,0 +1,80 @@
+def create_masks(inp, tar):
+    enc_padding_mask = create_padding_mask(inp)
+    dec_padding_mask = create_padding_mask(inp)
+
+    look_ahead_mask = create_look_ahead_mask(tf.shape(tar)[1])
+    dec_target_padding_mask = create_padding_mask(tar)
+    combined_mask = tf.maximum(dec_target_padding_mask, look_ahead_mask)
+  
+    return enc_padding_mask, combined_mask, dec_padding_mask
+
+checkpoint_path = "checkpoints"
+optimizer
+ckpt = tf.train.Checkpoint(transformer=transformer, optimizer=optimizer)
+
+ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
+
+if ckpt_manager.latest_checkpoint:
+    ckpt.restore(ckpt_manager.latest_checkpoint)
+    print ('Latest checkpoint restored!!')
+
+@tf.function
+def train_step(inp, tar):
+    tar_inp = tar[:, :-1]
+    tar_real = tar[:, 1:]
+
+    enc_padding_mask, combined_mask, dec_padding_mask = create_masks(inp, tar_inp)
+
+    with tf.GradientTape() as tape:
+        predictions, _ = transformer(
+            inp, tar_inp, 
+            True, 
+            enc_padding_mask, 
+            combined_mask, 
+            dec_padding_mask
+        )
+        loss = loss_function(tar_real, predictions)
+
+    gradients = tape.gradient(loss, transformer.trainable_variables)    
+    optimizer.apply_gradients(zip(gradients, transformer.trainable_variables))
+
+    train_loss(loss)
+    train_accuracy(accuracy_function(tar_real, predictions))
+
+
+def evaluate(input_article):
+    input_article = article_tokenizer.texts_to_sequences([input_article])
+    input_article = tf.keras.preprocessing.sequence.pad_sequences(input_article, maxlen=ENCODER_LEN, 
+                                                                   padding='post', truncating='post')
+
+    encoder_input = tf.expand_dims(input_article[0], 0)
+
+    decoder_input = [summary_tokenizer.word_index['<sos>']]
+    output = tf.expand_dims(decoder_input, 0)
+    
+    for i in range(DECODER_LEN):
+        enc_padding_mask, combined_mask, dec_padding_mask = create_masks(encoder_input, output)
+
+        predictions, attention_weights = transformer(
+            encoder_input, 
+            output,
+            False,
+            enc_padding_mask,
+            combined_mask,
+            dec_padding_mask
+        )
+
+        predictions = predictions[: ,-1:, :]
+        predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
+
+        if predicted_id == summary_tokenizer.word_index['<eos>']:
+            return tf.squeeze(output, axis=0), attention_weights
+
+        output = tf.concat([output, predicted_id], axis=-1)
+
+    return tf.squeeze(output, axis=0), attention_weights
+
+def summarize(input_article):
+    summarized = evaluate(input_article=input_article)[0].numpy()
+    summarized = np.expand_dims(summarized[1:], 0)  
+    return summary_tokenizer.sequences_to_texts(summarized)[0]
